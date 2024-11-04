d99cbbe7234d62b7fb6325ab7493d00c1848355d
bhaskarkrjha <bhaskarkumar228@gmail.com>
2024-11-04 19:58:12 +0530

    email-list changes

--- src/app/modules/email-list/email-list.component.ts
+++ src/app/modules/email-list/email-list.component.ts
@@ -1,10 +1,91 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { COMMA, ENTER, SPACE } from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'app-email-list',
   templateUrl: './email-list.component.html',
   styleUrl: './email-list.component.scss'
 })
-export class EmailListComponent {
+export class EmailListComponent implements OnInit {
+  sendEmail: FormGroup;
+  public emailList: any[] = [];
+  public emailpass: any[] = [];
+  sendDisabled = true;
+  @ViewChild('chipList') chipList;
+  emailNotValidTO: boolean;
+  separatorKeysCodes = [ENTER, COMMA, SPACE];
+  showEmail: boolean = false;
+  constructor(
+    private formBuilder : FormBuilder
+  ){}
+  ngOnInit(): void {
+    this.sendEmail = this.formBuilder.group({
+      email: [null, [Validators.required]],
+    });   
+  }
+  private validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+  add(event): void {
+    if (event.value) {
+      if (this.validateEmail(event.value) && this.emailpass.indexOf(event.value) === -1) {
+        this.emailList.push({ value: event.value, invalid: false });
+        this.emailpass.push(event.value);
+        this.chipList.errorState = false;
+        this.emailNotValidTO = false;
+      } else {
+        this.emailList.push({ value: event.value, invalid: true });
+      }
+    }
+    if (event.input) {
+      event.input.value = '';
+    }
+    this.sendEnable();
+  }
+
+
+paste(event: ClipboardEvent): void {
+    event.preventDefault();
+    event.clipboardData
+      .getData('Text')
+      .split(/;|,|\n|\r/)
+      .forEach((value) => {
+        if (this.validateEmail(value) && this.emailpass.indexOf(value) === -1) {
+          this.emailList.push({ value: value, invalid: false });
+          this.emailpass.push(value);
+        }
+      });
+    this.sendEnable();
+  }
+
+removeEmail(data: any): void {
+    if (this.emailList?.indexOf(data) >= 0) {
+      this.emailList?.splice(this.emailList?.indexOf(data), 1);
+      if (!data.invalid) {
+        this.emailpass?.splice(this.emailpass?.indexOf(data?.value), 1);
+      }
+    }
+    this.sendEnable();
+  }
+
+  sendEnable() {
+    this.sendDisabled = this.emailList.filter((email) => email.invalid).length > 0;
+    if (this.sendDisabled) {
+      this.chipList.errorState = true;
+      this.emailNotValidTO = true;
+    } else {
+      this.chipList.errorState = false;
+      this.emailNotValidTO = false;
+    }
+  }
+  viewList(){
+    this.showEmail = true;
+    console.log(this.emailList);
+    
+  }
+
+
 
 }
--- src/app/modules/email-list/email-list.module.ts
+++ src/app/modules/email-list/email-list.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmailListComponent } from './email-list.component';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import {MatChipEditedEvent, MatChipInputEvent, MatChipsModule} from '@angular/material/chips';
+import {MatButtonModule} from '@angular/material/button';
 const routes: Routes = [{ path: '', component: EmailListComponent }];
 
 @NgModule({
   declarations: [EmailListComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatChipsModule,
+    MatInputModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterModule.forChild(routes),
+  ],
 })
 export class EmailListModule {}
